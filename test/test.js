777f72d92bef68cdf3f5cda03b8c908558545e8f
David L. Day <dday376@gmail.com>
2018-12-23 20:35:47 -0500

    Fixed var usage.

--- test/test.js
+++ test/test.js
@@ -7,19 +7,18 @@ var fs = require('fs');
 describe("#md()", function () {
 
   it('should return the expected annotated text object', function () {
-    var expected = JSON.parse(fs.readFileSync('./test/annotatedtext/markdown.json', 'utf8'));
-    var result = builder.md(fs.readFileSync('./test/test.md', 'utf8'));
+    const expected = JSON.parse(fs.readFileSync('./test/annotatedtext/markdown.json', 'utf8'));
+    const result = builder.md(fs.readFileSync('./test/test.md', 'utf8'));
     expect(result).to.deep.equal(expected);
   });
 
   it('should match the original document exactly', function () {
-    var expected = fs.readFileSync('./test/test.md', 'utf8');
-    var annotatedtext = builder.md(expected);
-    var annotation = annotatedtext.annotation
-    var result = '';
-    for (var i = 0; i < annotation.length; i++) {
-      var node = annotation[i];
-      var text = node.text ? node.text : node.markup;
+    const expected = fs.readFileSync('./test/test.md', 'utf8');
+    const annotatedtext = builder.md(expected);
+    const annotation = annotatedtext.annotation
+    let result = '';
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
       result += text;
     }
     expect(result).to.equal(expected);
@@ -30,19 +29,18 @@ describe("#md()", function () {
 describe("#html()", function () {
 
   it('should return the expected annotated text object', function () {
-    var expected = JSON.parse(fs.readFileSync('./test/annotatedtext/html.json', 'utf8'));
-    var result = builder.html(fs.readFileSync('./test/test.html', 'utf8'));
+    const expected = JSON.parse(fs.readFileSync('./test/annotatedtext/html.json', 'utf8'));
+    const result = builder.html(fs.readFileSync('./test/test.html', 'utf8'));
     expect(result).to.deep.equal(expected);
   });
 
   it('should match the original document exactly', function () {
-    var expected = fs.readFileSync('./test/test.html', 'utf8');
-    var annotatedtext = builder.html(expected);
-    var annotation = annotatedtext.annotation
-    var result = '';
-    for (var i = 0; i < annotation.length; i++) {
-      var node = annotation[i];
-      var text = node.text ? node.text : node.markup;
+    const expected = fs.readFileSync('./test/test.html', 'utf8');
+    const annotatedtext = builder.html(expected);
+    const annotation = annotatedtext.annotation
+    let result = '';
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
       result += text;
     }
     expect(result).to.equal(expected);
