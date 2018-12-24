b8d9eba287e5557ab685fbd389fe8bfdd6ea41f5
David L. Day <dday376@gmail.com>
2018-12-23 20:55:00 -0500

    ' -> "

--- test/test.js
+++ test/test.js
@@ -1,22 +1,22 @@
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var builder = require('../index');
-var fs = require('fs');
+var expect = require("chai").expect;
+var builder = require("../index");
+var fs = require("fs");
 
 describe("#md()", function () {
 
-  it('should return the expected annotated text object', function () {
-    const expected = JSON.parse(fs.readFileSync('./test/annotatedtext/markdown.json', 'utf8'));
-    const result = builder.md(fs.readFileSync('./test/test.md', 'utf8'));
+  it("should return the expected annotated text object", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext/markdown.json", "utf8"));
+    const result = builder.md(fs.readFileSync("./test/test.md", "utf8"));
     expect(result).to.deep.equal(expected);
   });
 
-  it('should match the original document exactly', function () {
-    const expected = fs.readFileSync('./test/test.md', 'utf8');
+  it("should match the original document exactly", function () {
+    const expected = fs.readFileSync("./test/test.md", "utf8");
     const annotatedtext = builder.md(expected);
     const annotation = annotatedtext.annotation
-    let result = '';
+    let result = "";
     for (let node of annotation) {
       const text = node.text ? node.text : node.markup;
       result += text;
@@ -28,17 +28,17 @@ describe("#md()", function () {
 
 describe("#html()", function () {
 
-  it('should return the expected annotated text object', function () {
-    const expected = JSON.parse(fs.readFileSync('./test/annotatedtext/html.json', 'utf8'));
-    const result = builder.html(fs.readFileSync('./test/test.html', 'utf8'));
+  it("should return the expected annotated text object", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext/html.json", "utf8"));
+    const result = builder.html(fs.readFileSync("./test/test.html", "utf8"));
     expect(result).to.deep.equal(expected);
   });
 
-  it('should match the original document exactly', function () {
-    const expected = fs.readFileSync('./test/test.html', 'utf8');
+  it("should match the original document exactly", function () {
+    const expected = fs.readFileSync("./test/test.html", "utf8");
     const annotatedtext = builder.html(expected);
     const annotation = annotatedtext.annotation
-    let result = '';
+    let result = "";
     for (let node of annotation) {
       const text = node.text ? node.text : node.markup;
       result += text;
