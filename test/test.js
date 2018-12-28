6f5e9a16999a998f34db597eff80e224a20473b3
David L. Day <dday376@gmail.com>
2018-12-28 18:57:49 -0500

    override interpreter for markdown in tests.

--- test/test.js
+++ test/test.js
@@ -6,12 +6,18 @@ var remarkparse = require("remark-parse");
 var builder = require("../index");
 var fs = require("fs");
 
+var options = builder.defaults;
+options.interpretmarkup = function (text) {
+  let count = (text.match(/\n/g) || []).length;
+  return "\n".repeat(count);
+}
+
 describe("#collecttextnodes()", function () {
 
   it("should return the expected array of text nodes", function () {
     const ast = JSON.parse(fs.readFileSync("./test/ast.json", "utf8"));
     const expected = JSON.parse(fs.readFileSync("./test/textnodes.json", "utf8"));
-    const result = builder.collecttextnodes(ast);
+    const result = builder.collecttextnodes(ast, options);
     expect(result).to.deep.equal(expected);
   });
 
@@ -23,7 +29,7 @@ describe("#composeannotation()", function () {
     const expected = JSON.parse(fs.readFileSync("./test/annotatedtext.json", "utf8"));
     const text = fs.readFileSync("./test/test.md", "utf8");
     const textnodes = JSON.parse(fs.readFileSync("./test/textnodes.json", "utf8"));
-    const result = builder.composeannotation(text, textnodes);
+    const result = builder.composeannotation(text, textnodes, options);
     expect(result).to.deep.equal(expected);
   });
 
@@ -36,7 +42,7 @@ describe("#build()", function () {
     const text = fs.readFileSync("./test/test.md", "utf8");
     const processor = unified()
       .use(remarkparse, { commonmark: true });
-    const result = builder.build(text, processor.parse);
+    const result = builder.build(text, processor.parse, options);
     expect(result).to.deep.equal(expected);
   });
 
@@ -44,7 +50,7 @@ describe("#build()", function () {
     const expected = fs.readFileSync("./test/test.md", "utf8");
     const processor = unified()
       .use(remarkparse, { commonmark: true });
-    const annotatedtext = builder.build(expected, processor.parse);
+    const annotatedtext = builder.build(expected, processor.parse, options);
     const annotation = annotatedtext.annotation;
     let result = "";
     for (let node of annotation) {
