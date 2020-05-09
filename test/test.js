b946eb013aa8bf30decda5fa16f5eeda51748f69
David L. Day <dday376@gmail.com>
2020-05-09 14:18:34 -0400

    add tests for escape character

--- test/test.js
+++ test/test.js
@@ -15,9 +15,10 @@ options.interpretmarkup = function (text) {
 describe("#collecttextnodes()", function () {
 
   it("should return the expected array of text nodes", function () {
+    const text = fs.readFileSync("./test/test.md", "utf8");
     const ast = JSON.parse(fs.readFileSync("./test/ast.json", "utf8"));
     const expected = JSON.parse(fs.readFileSync("./test/textnodes.json", "utf8"));
-    const result = builder.collecttextnodes(ast, options);
+    const result = builder.collecttextnodes(ast, text, options);
     expect(result).to.deep.equal(expected);
   });
 
@@ -48,6 +49,29 @@ describe("#build()", function () {
 
   it("should match the original document exactly", function () {
     const expected = fs.readFileSync("./test/test.md", "utf8");
+    const processor = unified()
+      .use(remarkparse, { commonmark: true });
+    const annotatedtext = builder.build(expected, processor.parse, options);
+    const annotation = annotatedtext.annotation;
+    let result = "";
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
+      result += text;
+    }
+    expect(result).to.equal(expected);
+  });
+
+  it("should return the expected annotated text with backslashes object", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/escape-character.json", "utf8"));
+    const text = fs.readFileSync("./test/escape-character.md", "utf8");
+    const processor = unified()
+      .use(remarkparse, { commonmark: true });
+    const result = builder.build(text, processor.parse, options);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should match the original document with backslashes exactly", function () {
+    const expected = fs.readFileSync("./test/escape-character.md", "utf8");
     const processor = unified()
       .use(remarkparse, { commonmark: true });
     const annotatedtext = builder.build(expected, processor.parse, options);
