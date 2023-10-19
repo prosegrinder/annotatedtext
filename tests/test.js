0167963e58656e3559754aab5389dbd06de82844
David L. Day <dday376@gmail.com>
2023-10-19 07:32:20 -0400

    test: replaced build tests with compose tests

--- tests/test.js
+++ tests/test.js
@@ -45,36 +45,90 @@ describe("#composeannotation()", function () {
   });
 });
 
-describe("#build()", function () {
+// describe("#build()", function () {
+//   it("should return the expected annotated text object", function () {
+//     const expected = JSON.parse(
+//       fs.readFileSync("./tests/annotatedtext.json", "utf8"),
+//     );
+//     const text = fs.readFileSync("./tests/test.md", "utf8");
+//     const processor = unified().use(remarkparse, { commonmark: true });
+//     const result = builder.build(text, processor.parse, options);
+//     // fs.writeFileSync(
+//     //   "./out/ts/annotatedtext-build.json",
+//     //   JSON.stringify(result),
+//     //   "utf8",
+//     // );
+//     chai.expect(result).to.deep.equal(expected);
+//   });
+
+//   it("should match the original document exactly", function () {
+//     const expected = fs.readFileSync("./tests/test.md", "utf8");
+//     const processor = unified().use(remarkparse, {
+//       commonmark: false,
+//       gfm: true,
+//     });
+//     const annotatedtext = builder.build(expected, processor.parse, options);
+//     const annotation = annotatedtext.annotation;
+//     let result = "";
+//     for (let node of annotation) {
+//       const text = node.text ? node.text : node.markup;
+//       result += text;
+//     }
+//     // fs.writeFileSync("./out/ts/test.md", result, "utf8");
+//     chai.expect(result).to.equal(expected);
+//   });
+
+//   it("should return the expected annotated text with backslashes object", function () {
+//     const expected = JSON.parse(
+//       fs.readFileSync("./tests/escape-character.json", "utf8"),
+//     );
+//     const text = fs.readFileSync("./tests/escape-character.md", "utf8");
+//     const processor = unified().use(remarkparse, { commonmark: true });
+//     const result = builder.build(text, processor.parse, options);
+//     // fs.writeFileSync(
+//     //   "./out/ts/escape-character.json",
+//     //   JSON.stringify(result),
+//     //   "utf8",
+//     // );
+//     chai.expect(result).to.deep.equal(expected);
+//   });
+
+//   it("should match the original document with backslashes exactly", function () {
+//     const expected = fs.readFileSync("./tests/escape-character.md", "utf8");
+//     const processor = unified().use(remarkparse, { commonmark: true });
+//     const annotatedtext = builder.build(expected, processor.parse, options);
+//     const annotation = annotatedtext.annotation;
+//     let result = "";
+//     for (let node of annotation) {
+//       const text = node.text ? node.text : node.markup;
+//       result += text;
+//     }
+//     // fs.writeFileSync("./out/ts/escape-character.md", result, "utf8");
+//     chai.expect(result).to.equal(expected);
+//   });
+// });
+
+describe("#compose()", function () {
   it("should return the expected annotated text object", function () {
     const expected = JSON.parse(
       fs.readFileSync("./tests/annotatedtext.json", "utf8"),
     );
     const text = fs.readFileSync("./tests/test.md", "utf8");
-    const processor = unified().use(remarkparse, { commonmark: true });
-    const result = builder.build(text, processor.parse, options);
-    // fs.writeFileSync(
-    //   "./out/ts/annotatedtext-build.json",
-    //   JSON.stringify(result),
-    //   "utf8",
-    // );
+    const nodes = JSON.parse(fs.readFileSync("./tests/nodes.json", "utf8"));
+    const result = builder.compose(text, nodes, options);
     chai.expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly", function () {
     const expected = fs.readFileSync("./tests/test.md", "utf8");
-    const processor = unified().use(remarkparse, {
-      commonmark: false,
-      gfm: true,
-    });
-    const annotatedtext = builder.build(expected, processor.parse, options);
+    const nodes = JSON.parse(fs.readFileSync("./tests/nodes.json", "utf8"));
+    const annotatedtext = builder.compose(expected, nodes, options);
     const annotation = annotatedtext.annotation;
     let result = "";
     for (let node of annotation) {
       const text = node.text ? node.text : node.markup;
       result += text;
     }
-    // fs.writeFileSync("./out/ts/test.md", result, "utf8");
     chai.expect(result).to.equal(expected);
   });
 
@@ -83,27 +137,25 @@ describe("#build()", function () {
       fs.readFileSync("./tests/escape-character.json", "utf8"),
     );
     const text = fs.readFileSync("./tests/escape-character.md", "utf8");
-    const processor = unified().use(remarkparse, { commonmark: true });
-    const result = builder.build(text, processor.parse, options);
-    // fs.writeFileSync(
-    //   "./out/ts/escape-character.json",
-    //   JSON.stringify(result),
-    //   "utf8",
-    // );
+    const nodes = JSON.parse(
+      fs.readFileSync("./tests/escape-character-nodes.json", "utf8"),
+    );
+    const result = builder.compose(text, nodes, options);
     chai.expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document with backslashes exactly", function () {
     const expected = fs.readFileSync("./tests/escape-character.md", "utf8");
-    const processor = unified().use(remarkparse, { commonmark: true });
-    const annotatedtext = builder.build(expected, processor.parse, options);
+    const nodes = unified()
+      .use(remarkparse, { commonmark: true })
+      .parse(expected);
+    const annotatedtext = builder.compose(expected, nodes, options);
     const annotation = annotatedtext.annotation;
     let result = "";
     for (let node of annotation) {
       const text = node.text ? node.text : node.markup;
       result += text;
     }
-    // fs.writeFileSync("./out/ts/escape-character.md", result, "utf8");
     chai.expect(result).to.equal(expected);
   });
 });
